8a820ce7f7377607c609c915ab883046a720bf8b
gar354 <115901590+gar354@users.noreply.github.com>
2024-02-29 20:47:00 -0800

    add setters and getters

--- Thingy.java
+++ Thingy.java
@@ -1,9 +1,22 @@
-class Thingy
-{
-  private int myX, myY;
-  public Thingy()
-  {
-    myX = myY = 0;
+class Thingy {
+  private int x, y;
+  public Thingy() {
+    x = y = 0;
   }
-  /* your java code for "getters" and "setters" here */
+
+  public void setX(int x_) {
+    x = x_;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public void setY(int y_) {
+    y = y_;
+  }
+
+  public int getY() {
+    return y;
+  }
 }
